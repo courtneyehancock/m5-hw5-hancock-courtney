bac8b19ca1d7c582a26eacbe755254d3fdb51006
Courtney Hancock <ceh.courtney@gmail.com>
2019-06-14 16:51:34 -0700

    Add hover event listeners to each button for color

---
+++ main.js
@@ -0,0 +1,38 @@
+//first button
+document.getElementById("one1").addEventListener("mouseenter",
+  function() {
+    document.getElementById("one1").style.backgroundColor = "#ffff00";
+  }, false
+);
+
+document.getElementById("one1").addEventListener("mouseleave",
+  function() {
+    document.getElementById("one1").style.backgroundColor = "#ffffe0";
+  }, false
+);
+
+//second button
+document.getElementById("two2").addEventListener("mouseenter",
+  function() {
+    document.getElementById("two2").style.backgroundColor = "#003366";
+  }, false
+);
+
+document.getElementById("two2").addEventListener("mouseleave",
+  function() {
+    document.getElementById("two2").style.backgroundColor = "#add8e6";
+  }, false
+);
+
+//third button
+document.getElementById("three3").addEventListener("mouseenter",
+  function() {
+    document.getElementById("three3").style.backgroundColor = "#008000";
+  }, false
+);
+
+document.getElementById("three3").addEventListener("mouseleave",
+  function() {
+    document.getElementById("three3").style.backgroundColor = "#90ee90";
+  }, false
+);
